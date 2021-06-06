1af639c9520d1a19ce628eb75cbb4e783dda49b5
Sachman19 <sachindmishra@knights.ucf.edu>
2021-06-06 05:24:56 -0400

    Version 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,6 +10,17 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        System.out.print("How many euros are you exchanging? ");
+        double euros = input.nextDouble();
+
+        System.out.print("What is the exchange rate? ");
+        double rate = input.nextDouble();
+
+        double usd = Math.round(100 * euros * rate);
+        usd /= 100;
+
+        System.out.println();
+        System.out.printf("%.2f euros at an exchange rate of %f is %.2f", euros, rate, usd);
 
     }
 }
